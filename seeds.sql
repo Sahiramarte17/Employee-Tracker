12f0f5f1ccf2c67028283a6114ff74d861a207b1
Sahira Marte <sahira@sahiras-mbp.myfiosgateway.com>
2024-10-01 18:45:51 -0400

    Adding roles values

--- seeds.sql
+++ seeds.sql
@@ -2,6 +2,10 @@ INSERT INTO department (name)
 VALUES ('Engineering'), ('Sales'), ('HR');
 
 -- INSERT INTO role
-
+INSERT INTO role (title, salary, department_id) 
+VALUES ('Sales Manager', 60000, 1), 
+       ('Sales Associate', 40000, 1), 
+       ('Software Engineer', 80000, 2), 
+       ('HR Specialist', 50000, 3);
 
 -- INSERT INTO employee
