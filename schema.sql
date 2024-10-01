509985a1d50e43800901927bb4b05ccf160483be
Sahira Marte <sahira@sahiras-mbp.myfiosgateway.com>
2024-10-01 18:29:34 -0400

    Adding role table

--- schema.sql
+++ schema.sql
@@ -3,15 +3,17 @@ CREATE DATABASE employee Cascade;
 
 \c employee_tracker_db;
 
+-- Create departments table
 CREATE TABLE department (
     id SERIAL PRIMARY KEY,
     name VARCHAR(30) UNIQUE NOT NULL
 );
 
+-- Create roles table
 CREATE TABLE role (
     id SERIAL PRIMARY KEY
     tittle VARCHAR(30) UNIQUE NOT NULL
-    salry
+    salary DECIMAL NOT NULL
 
 -- );
 
