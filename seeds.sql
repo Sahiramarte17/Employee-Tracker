cc0ff1e89a3129ca9696a42971904e88b25840e4
Sahira Marte <sahira@sahiras-mbp.myfiosgateway.com>
2024-10-01 18:47:10 -0400

    Adding values to employees name

--- seeds.sql
+++ seeds.sql
@@ -9,3 +9,8 @@ VALUES ('Sales Manager', 60000, 1),
        ('HR Specialist', 50000, 3);
 
 -- INSERT INTO employee
+INSERT INTO employee (first_name, last_name, role_id, manager_id) 
+VALUES ('Alice', 'Johnson', 1, NULL),
+       ('Bob', 'Smith', 2, 1),
+       ('Charlie', 'Brown', 3, NULL),
+       ('David', 'Wilson', 4, NULL);
