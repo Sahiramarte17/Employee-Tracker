caea8f6865bcdd25d8ef39612160677662e0b293
Sahira Marte <sahira@sahiras-mbp.myfiosgateway.com>
2024-10-01 18:33:24 -0400

    Adding table for employee

--- schema.sql
+++ schema.sql
@@ -14,9 +14,16 @@ CREATE TABLE role (
     id SERIAL PRIMARY KEY
     tittle VARCHAR(30) UNIQUE NOT NULL
     salary DECIMAL NOT NULL
+    department_id INTEGER NOT NULL REFERENCES department(id)
 
--- );
+ );
 
--- CREATE TABLE employee (
+-- CREATE TABLE employee 
+CREATE TABLE employee (
+    id SERIAL PRIMARY KEY,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    role_id INTEGER NOT NULL REFERENCES role(id),
+    manager_id INTEGER REFERENCES employee(id) 
+);
 
--- );
